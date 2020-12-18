1e0ef031d24067079afdb8315ef2e50eef68240f
hasibulhasanshanto <kajal.shanto@gmail.com>
2020-12-18 16:05:41 +0600

    done string

---
+++ 5. Strings/5.1_strings.php
@@ -0,0 +1,41 @@
+<?php 
+
+## Simple string
+$string = 'Hasibul';
+$string2 = "Hasan";
+
+## String Concetanation
+echo 'Hello '.$string.'! Welcome here!<br>';
+echo "Your second name is: $string2";
+echo "<br><br>";
+
+## String Function
+$example = "    Hello World!    ";
+
+echo strlen($example).'<br>';
+echo trim($example).'<br>';
+echo ltrim($example).'<br>';
+echo rtrim($example).'<br>';
+echo strrev($example).'<br>';
+echo strtoupper($example).'<br>';
+echo strtolower($example).'<br>';
+
+echo strpos("Hello world!", "world").'<br>'; // outputs 6
+echo str_replace("world", "Dolly", "Hello world!").'<br>'; // outputs Hello Dolly!
+
+echo crypt('123456','st').'<br>';
+echo lcfirst("Hello world!").'<br>';
+echo ucfirst("hello world!").'<br>';
+echo ucwords("hello world").'<br>';
+
+echo "<br><br>";
+$str = "An example of a long word is: Supercalifragulistic";
+echo wordwrap($str,15,"<br>\n");
+
+echo "<br><br>";
+$str = "Hello";
+echo md5($str);
+
+
+
+?>
