1e66a1445820ec7770cf95f42db1815801dd8f4a
hasibulhasanshanto <kajal.shanto@gmail.com>
2020-12-18 15:40:28 +0600

    update math

---
+++ 7. Math/7.1_math.php
@@ -0,0 +1,26 @@
+<?php
+
+### PHP Math functions
+
+    echo(pi()).'<br>'; // returns 3.1415926535898
+    echo(min(0, 150, 30, 20, -8, -200)).'<br>';  // returns -200
+    echo(max(0, 150, 30, 20, -8, -200)).'<br>';  // returns 150
+    echo(abs(-6.7)).'<br>';  // returns 6.7
+    echo(sqrt(64)).'<br>';  // returns 8
+    echo(pow(2, 3)).'<br>';  // returns 8
+    echo(round(0.60)).'<br>';  // returns 1
+    echo(round(0.49)).'<br>';  // returns 0
+    echo(rand()).'<br>';
+    echo(rand(10, 100)).'<br>';
+    echo(ceil(5.1) . "<br>");
+    echo(ceil(5.9)).'<br>';
+    echo(floor(5.1) . "<br>");
+    echo(floor(5.9)).'<br>';
+
+    /**
+     * Refference for More:
+     * https://www.w3schools.com/php/php_ref_math.asp
+     */
+
+
+?>
